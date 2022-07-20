e8a936a7f8c5cb04f7a9b77382904e21adbe9707
Matt <eclecticappdevelopment@gmail.com>
2022-07-20 12:37:02 +0100

    Now outputs in table format with descriptions

--- Get-DinosaurFight.ps1
+++ Get-DinosaurFight.ps1
@@ -1,4 +1,4 @@
-﻿
+
 function replaceText{
 	param (
 		$text,
@@ -16,13 +16,15 @@ function replaceText{
 }
 
 #START OUTPUT
-#$output = """'<h1>Dino Fight!</h1>";
 $output = "'<h1>Dino Fight!</h1>";
 $output = -join($output, "<b>Who will win in this match up between:</b>");
-$output = -join($output, "<table><tr>");
+$output = -join($output, "<br><br>");
+$output = -join($output, "<table>");
+
+#SAME URL
+$url = "https://dinosaur-facts-api.shultzlab.com/dinosaurs/random";
 
 #DINO #1
-$url = "https://dinosaur-facts-api.shultzlab.com/dinosaurs/random";
 #CALL THE API TO GET THE JSON
 $json = Invoke-WebRequest $url;
 #PREPARE A HASHTABLE
@@ -30,55 +32,87 @@ $hashtable = @{}
 #CONVERT THE JSON TO A HASHTABLE
 (ConvertFrom-Json $json).psobject.properties | ForEach-Object { $hashtable[$_.Name] = $_.Value }
 $nameOne = replaceText($hashtable['Name']);
-$output = -join($output, "<td><b>$nameOne</b></td>");
+$descOne = replaceText($hashTable['Description']);
 #GET GOOGLE IMAGES
 $content = Invoke-RestMethod "https://www.google.com/search?q=$nameOne+dinosaur&sxsrf=ALiCzsZ3iOWI-1dHX8uZnJkdXDfHkRSHtw:1658304343535&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiMnc2KgYf5AhV5QUEAHUp8AAkQ_AUoAXoECAEQAw&cshid=1658304366384794&biw=1368&bih=769&dpr=2";
 #FIND THE FIRST <img>
 $content -match '<img class="yWs4tf".*src="(?<imgSrc>.*)"/>' > $silent;
 #GENERATE A NEW IMAGE TAG
-$img = "<img width=200 height=200 src=";
-$img = -join($img, $Matches.imgSrc);
-$img = -join($img, " />");
-#APPEND TO DINO FIGHT TABLE
-$output = -join($output, "<td>");
-$output = -join($output, $img);
-$output = -join($output, "</td>");
-$output = -join($output, "</tr>");
-
-#ADD A ROW SHOWING "AND"
-$output = -join($output, "<tr><td>AND</td></td></tr>");
+$imgOne = "<img width=200 height=200 src=";
+$imgOne = -join($imgOne, $Matches.imgSrc);
+$imgOne = -join($imgOne, " />");
 
 #DINO #2
-$output = -join($output, "<tr>");
-#$output = -join($output, "<td>");
-#CALL JSON
 $json = Invoke-WebRequest $url;
 #PREPARE A HASHTABLE
 $hashtable = @{}
 #CONVERT THE JSON TO A HASHTABLE
 (ConvertFrom-Json $json).psobject.properties | ForEach-Object { $hashtable[$_.Name] = $_.Value }
 $nameTwo = replaceText($hashtable['Name']);
-$output = -join($output, "<td><b>$nameTwo</b></td>");
+$descTwo = replaceText($hashTable['Description']);
 #GET GOOGLE IMAGES
 $content = Invoke-RestMethod "https://www.google.com/search?q=$nameTwo+dinosaur&sxsrf=ALiCzsZ3iOWI-1dHX8uZnJkdXDfHkRSHtw:1658304343535&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiMnc2KgYf5AhV5QUEAHUp8AAkQ_AUoAXoECAEQAw&cshid=1658304366384794&biw=1368&bih=769&dpr=2";
 #FIND THE FIRST <img>
 $content -match '<img class="yWs4tf".*src="(?<imgSrc>.*)"/>' > $silent;
 #GENERATE A NEW IMAGE TAG
-$img = "<img width=200 height=200 src=";
-$img = -join($img, $Matches.imgSrc);
-$img = -join($img, " />");
-#APPEND TO DINO FIGHT TABLE
-$output = -join($output, "<td>");
-$output = -join($output, $img);
-$output = -join($output, "</td>");
+$imgTwo = "<img width=200 height=200 src=";
+$imgTwo = -join($imgTwo, $Matches.imgSrc);
+$imgTwo = -join($imgTwo, " />");
+
+
+#OUTPUT NAMES ROW
+$output = -join($output, "<tr>");
+    #NAME ONE
+    $output = -join($output, "<td><h2>");
+    $output = -join($output, $nameOne);
+    $output = -join($output, "</h2></td>");
+    #VS
+    $output = -join($output, "<td>");
+    $output = -join($output, " vs. ");
+    $output = -join($output, "</td>");
+    #NAME TWO
+    $output = -join($output, "<td><h2>");
+    $output = -join($output, $nameTwo);
+    $output = -join($output, "</h2></td>");
 $output = -join($output, "</tr>");
+
+#OUTPUT DESCRIPTION ROW
+$output = -join($output, "<tr>");
+    #NAME ONE
+    $output = -join($output, "<td>");
+    $output = -join($output, "<em>");
+    $output = -join($output, $descOne);
+    $output = -join($output, "</em>");
+    $output = -join($output, "</td>");
+    #EMPTY
+    $output = -join($output, "<td>");
+    $output = -join($output, "</td>");
+    #NAME TWO
+    $output = -join($output, "<td>");
+    $output = -join($output, "<em>");
+    $output = -join($output, $descTwo);
+    $output = -join($output, "</em>");
+    $output = -join($output, "</td>");
+$output = -join($output, "</tr>");
+
+#OUTPUT IMAGE ROW
+$output = -join($output, "<tr>");
+    #NAME ONE
+    $output = -join($output, "<td>");
+    $output = -join($output, $imgOne);
+    $output = -join($output, "</td>");
+    #EMPTY
+    $output = -join($output, "<td>");
+    $output = -join($output, "</td>");
+    #NAME TWO
+    $output = -join($output, "<td>");
+    $output = -join($output, $imgTwo);
+    $output = -join($output, "</td>");
+$output = -join($output, "</tr>");
+
 $output = -join($output, "</table>'");
-#$output = -join($output, "</table>'""");
 
 Write-Output $output;
-#exit 1;
-
-#$output = '"<table><tr><td><img width=200 height=200 src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdvG9xMqyPjJ6p1TZTqES-WBTflQ0HzTQPKtQhBtbI59eFbr07WJkzopxlOqM&s /></td></tr></table>"';
 
 #POST TO TEAMS
 Invoke-Expression -Command "C:\Users\matthew.tiernan\Desktop\POWERSHELL\utilities\Send-TeamsMessage.ps1 $output" > $silent; 
