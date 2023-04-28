48a5f16d6853ffafbadab11c345e97d804d3f497
Matt <eclecticappdevelopment@gmail.com>
2023-04-28 12:44:58 +0100

    New powershell daily utilities

---
+++ Get-AnimeQuote.ps1
@@ -0,0 +1,97 @@
+﻿<#
+	.Synopsis
+	Get the random anime quote and send via webhook
+	
+	.Version
+	1.0
+
+	.Description
+	Get-AnimeQuote gets an anime quote via API and posts to Teams.
+
+	.Link
+	https://animechan.vercel.app/docs#random-quote
+#>
+
+#IMPORT THE CLASSES
+. C:\Users\matthew.tiernan\Desktop\POWERSHELL\utilities\AdaptiveCards\Classes\Imports.ps1
+
+
+#BASE URLS
+$wordOfTheDayUrl = "https://animechan.vercel.app/api/random"; 
+
+#CALL THE API TO GET THE JSON
+$json = Invoke-WebRequest $wordOfTheDayUrl;
+
+#CHECK THE JSON RETURN (IN CASE API FAILS)
+Write-Output $json.Content;
+#pause;
+#exit 1;
+
+#PREPARE A HASHTABLE
+$hashtable = @{}
+
+#CONVERT THE JSON TO A HASHTABLE
+(ConvertFrom-Json $json).psobject.properties | ForEach-Object { $hashtable[$_.Name] = $_.Value }
+
+#THE WORD ITSELF IS THE TOP "word" - NOW CALLING THE REPLACE FUNCTION JUST IN BLOODY CASE
+$quote = $hashtable["quote"];
+$anime = $hashtable["anime"];
+$character = $hashtable["character"];
+
+#==========================
+# HEADING (WORD OF THE DAY)
+#==========================
+$headerText = [TextBlock]::new();
+$headerText.setText("Daily Anime Quote:");
+$headerText.setWeight('default');
+
+$headerWord = [TextBlock]::new();
+$headerWord.setText("$quote");
+$headerWord.setWeight('bolder');
+$headerWord.setSize('extraLarge');
+
+$animeText = [TextBlock]::new();
+$animeText.setSize('small');
+$animeText.setStyle('heading');
+$animeText.setText("Anime: $anime");
+$animeText.setWeight('bolder');
+
+$charText = [TextBlock]::new();
+$charText.setSize('small');
+$charText.setStyle('heading');
+$charText.setText("Character: $character");
+$charText.setWeight('bolder');
+
+
+#THE ENTIRE CONTENT IS ONE COLUMN/CONTAINER
+$content = @(
+	$headerText.out(), 
+	$headerWord.out(), 
+	$animeText.out(),
+	$charText.out()
+);
+
+
+
+#PUT THE ARRAY OF CONTENT IN A CONTAINER
+$fullContainer = [Container]::new();
+$fullContainer.setItems($content);
+
+#WRAP IN AN ADAPTIVE CARD
+[AdaptiveCard]$card = [AdaptiveCard]::new($fullContainer.out());
+
+#WRAP THE ADAPTIVE CARD IN A MESSAGE
+[Message]$message = [Message]::new($card.object);
+#STRINGIFY THE FINAL MESSAGE - THIS *MUST* USE THE out() METHOD
+$output = $message.out();
+
+#OUTPUT TO SCREEN FOR CHECKING
+Write-Output $output;
+#pause;
+#exit 1;
+
+#SAVE TO FILE (lastCardOutput.json)
+$output > "C:\Users\matthew.tiernan\Desktop\POWERSHELL\test\TeamsCards\lastCardOutput.json";
+
+#SEND TO TEAMS
+Invoke-Expression -Command "C:\Users\matthew.tiernan\Desktop\POWERSHELL\utilities\Send-TeamsMessage.ps1 `$output` ""true""" > $silent;
---
+++ Get-RandomGuitarTab.ps1
@@ -0,0 +1,170 @@
+﻿<#
+	.Synopsis
+	Get a random guitar tab and send via webhook
+	
+	.Version
+	1.0
+
+	.Description
+	Get-RandomGuitarTab gets a random guitar tab via API/Web Scraping and posts to Teams.
+
+	.Link
+	https://www.songsterr.com/sitemap-chords-1.xml
+#>
+
+#IMPORT THE CLASSES
+. C:\Users\matthew.tiernan\Desktop\POWERSHELL\utilities\AdaptiveCards\Classes\Imports.ps1
+
+#CLEAR SCREEN
+Cls;
+
+#GET A RANDOM PAGE ID
+$randomId = Get-Random -Maximum 15;
+#GET THE BASE PAGE + RANDOM ID, e.g. https://www.songsterr.com/sitemap-chords-1.xml
+$baseUrl = "https://www.songsterr.com/sitemap-chords-";
+$url = ($baseUrl + $randomId + ".xml");
+#CONFIRM THE URL TO GET
+Write-Output $url;
+
+#CALL THE API TO GET THE XML
+[xml]$xml = (Invoke-WebRequest $url).Content;
+
+#STORE THE COUNT OF SONGS
+$songCount = $xml.urlset.url.length;
+
+#GET A RANDOM SONG ID (UP TO THE NUMBER OF RESULTS IN THE XML
+$randomSongId = Get-Random -Maximum $songCount;
+
+#SELECT THIS SONG (FIRST 1 = 1 URL, SKIP $randomSongId URLS)
+$selected = ($xml.urlset.url | Select -First 1 -Skip $randomSongId).loc;
+
+#DEBUG OUTPUT
+Write-Output "The Selected Song is: ";
+Write-Output $selected;
+
+#LOAD THE CONTENT FROM THE SELECTED URL
+$html = (Invoke-WebRequest $selected).Content;
+
+#EXTRACT THE TITLE (FOR INFO, HIDDEN IN OUTPUT)
+$titleRegex = "<title>(?<title>[\w ]*) \|.*<\/title>";
+$html -match $titleRegex > $silent;
+if($null -ne $Matches){
+    Write-Output "The Selected Title found as: ";
+    Write-Output $Matches.title;
+}else{
+    Write-Output "cannot find title";
+}
+
+<#
+#EXTRACT THE CHORDS SECTION
+$chordsRegex = '<section id="chords" class="D0ez9">(?<chords>.*)<\/section>';
+$html -match $chordsRegex > $silent;
+if($null -ne $Matches){
+    Write-Output "Chords Found as: ";
+    $chords = $Matches.chords;
+}else{
+    Write-Output "Cannot find chords";
+}
+#>
+
+#https://learn.microsoft.com/en-us/microsoftteams/platform/task-modules-and-cards/cards/cards-format?tabs=adaptive-md%2Cdesktop%2Cconnector-html
+
+
+#https://microlink.io/docs/api/parameters/screenshot/fullPage
+
+#GET SCREENSHOT OF THE LINK
+$microlinkBase = "https://api.microlink.io/?url=";
+#$microlinkSuffix = "&screenshot=true";       #TOP-PART SCREENSHOT
+$microLinkSuffix = "&screenshot&fullPage";    #FULL PAGE SCREENSHOT?
+
+$microLink = ($microlinkBase + $selected + $microlinkSuffix);
+
+$microlinkJson = Invoke-WebRequest $microLink;
+
+$json = $microlinkJson.Content;
+
+$jsonObj = $json | ConvertFrom-Json;
+
+if($jsonObj.status -ne "success"){
+    Write-Output "Error getting screenshot data - exiting...";
+    exit 1;
+}
+
+$screenshotUrl = $jsonObj.data.screenshot.url;
+#Write-oUtput $url;
+
+#GENERATE AN IMAGE ELEMENT
+$image = [Image]::new();
+$image.setUrl($screenshotUrl);
+$image.setSize('extralarge');
+
+
+#==========================
+# HEADING (TAB OF THE DAY)
+#==========================
+#GET THE CURRENT DAY AND MONTH IN TEXT FOR HEADER OUTPUT
+$month = (Get-Date).ToString("MM");
+$monthName = (Get-Culture).DateTimeFormat.GetMonthName($month);
+$day = (Get-Date).ToString("dd");
+
+$headerText = [TextBlock]::new();
+$headerText.setText("Tab of the Day");
+$headerText.setWeight('default');
+
+$headerWord = [TextBlock]::new();
+$headerWord.setText("$day $monthName");
+$headerWord.setWeight('bolder');
+$headerWord.setSize('extraLarge');
+
+#PREPARE A CONTAINER TO STORE DEFINITIONS
+#$eventsContainer = [Container]::new();
+
+<#
+#==========================
+# PASTE CHORDS
+#==========================
+$chordBlock = [TextBlock]::new();
+$chordBlock.setText($chords);
+$chordBlock.setSize('small');
+#$thisEvent.setHorizontalAlignment('left');
+	
+#ADD TO THE DEFINITION CONTAINER
+$eventsContainer.addItem($chordBlock.out());
+#>
+
+#THE ENTIRE CONTENT IS ONE COLUMN/CONTAINER
+$content = @(
+	$headerText.out(), 
+	$headerWord.out(), 
+    $image.out()
+	#$eventsContainer.out()
+);
+
+#ADD ACTION.OPENURL
+$action = [ActionOpenUrl]::new($selected,"View this tab on Songsterr");
+
+#PUT THE ARRAY OF CONTENT IN A CONTAINER
+$fullContainer = [Container]::new();
+$fullContainer.setItems($content);
+$fullContainer.setSelectAction($action.out());
+
+#WRAP IN AN ADAPTIVE CARD
+[AdaptiveCard]$card = [AdaptiveCard]::new($fullContainer.out());
+
+#WRAP THE ADAPTIVE CARD IN A MESSAGE
+[Message]$message = [Message]::new($card.object);
+#STRINGIFY THE FINAL MESSAGE - THIS *MUST* USE THE out() METHOD
+$output = $message.out();
+
+#SAVE TO FILE (lastCardOutput.json)
+$output > "C:\Users\matthew.tiernan\Desktop\POWERSHELL\test\TeamsCards\lastCardOutput.json";
+
+#OUTPUT TO SCREEN FOR CHECKING
+Write-Output $output;
+#pause;
+
+#DEBUG - PREVENT TEAMS SEND
+#exit 1;
+
+#SEND TO TEAMS
+Invoke-Expression -Command "C:\Users\matthew.tiernan\Desktop\POWERSHELL\utilities\Send-TeamsMessage.ps1 `$output` ""true""" > $silent;
